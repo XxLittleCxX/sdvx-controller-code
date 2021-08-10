cbbb5058ac2bb76abbdaffac77cb0c3099afa4f2
LittleC <i@ltlec.cn>
2021-08-10 13:17:50 +0800

    :zap: use marco

--- sdvx-controller-code.ino
+++ sdvx-controller-code.ino
@@ -1,10 +1,9 @@
 #include <Encoder.h>
 #include "HID-Project.h"
 
+#define USE_GAMEPAD
 Encoder knobLeft(0, 1);
 Encoder knobRight(2,3);
-#define VOL_L_SENSE 0.25
-#define VOL_R_SENSE 0.25
 // BT ABCD FX LR START
 uint8_t buttonPins[] = {5,6,7,8,9,10,11};
 uint8_t buttonLights[] = {A0,A1,A2,A3,A4,A5,13};
@@ -12,8 +11,12 @@ char buttonKeys[] = {'d','f','j','k','c','m',' '};
 uint8_t buttonPressed[] = {false,false,false,false,false,false,false};
 
 void setup() {
-  Mouse.begin();
-  Keyboard.begin();
+  #ifdef USE_GAMEPAD
+Gamepad.begin();
+  #else
+Mouse.begin();
+Keyboard.begin();
+  #endif
   for(auto pin: buttonPins){
     pinMode(pin, INPUT_PULLUP);
   }
@@ -30,9 +33,14 @@ void loop() {
   long newLeft, newRight;
   newLeft = knobLeft.read();
   newRight = knobRight.read();
-  Mouse.move((positionLeft - newLeft) * VOL_L_SENSE, (newRight - positionRight) * VOL_R_SENSE);
+  #ifdef USE_GAMEPAD
+  Gamepad.xAxis(newLeft * 70);
+  Gamepad.yAxis(newRight * 70);
+  #else
+  Mouse.move((positionLeft - newLeft) * 0.25, (newRight - positionRight) * 0.25);
   positionLeft = newLeft;
   positionRight = newRight;
+  #endif
 
   for(int i=0;i<7;i++){
     if(buttonPressed[i]){
@@ -41,13 +49,21 @@ void loop() {
       digitalWrite(buttonLights[i], 0);
     }
     auto read = digitalRead(buttonPins[i]);
-    Serial.println(read);
     if(read == LOW && !buttonPressed[i]){
+      #ifdef USE_GAMEPAD
+      Gamepad.press(i+1);
+      #else
       Keyboard.press(buttonKeys[i]);
+      #endif
       buttonPressed[i] = true;
     }else if(read == HIGH && buttonPressed[i]){
+      #ifdef USE_GAMEPAD
+      Gamepad.release(i+1);
+      #else
       Keyboard.release(buttonKeys[i]);
+      #endif
       buttonPressed[i] = false;
     }
   }
+  Gamepad.write();
 }
